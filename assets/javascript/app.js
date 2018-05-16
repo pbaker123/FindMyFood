786a2398d39f14101bf5c372490a4f99eccbf517
sastolarski <sastolarski@gmail.com>
2018-05-16 18:35:52 -0500

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD6x5BYfR2yV9_yS-pYsDZ8RrU7ef3U56o",
+    authDomain: "findmyfood-869fa.firebaseapp.com",
+    databaseURL: "https://findmyfood-869fa.firebaseio.com",
+    projectId: "findmyfood-869fa",
+    storageBucket: "findmyfood-869fa.appspot.com",
+    messagingSenderId: "811192802841"
+  };
+  firebase.initializeApp(config);
