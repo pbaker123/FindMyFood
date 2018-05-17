af958ce09ee80c81c25c7b969c15e83435dbf9eb
ruben-r <ruben.ruiz@outlook.com>
2018-05-16 19:41:00 -0500

    added a working geolocation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,5 +11,33 @@
 
   // initial Gmap call with our api.  Take a look at this example: https://developers.google.com/maps/documentation/javascript/examples/map-simple
 
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEnHfgL17CU3dvMKKGW9kOuRHLcYZ7EQ8&callback=[function-call]"
-    async defer></script>
+
+    function initMap() {
+
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          console.log(pos);
+          $("#location").html("your location is lat: " + pos.lat + " long: " + pos.lat);
+        }, function() {
+          handleLocationError(true, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, map.getCenter());
+      }
+    }
+
+    var queryURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEnHfgL17CU3dvMKKGW9kOuRHLcYZ7EQ8&callback=" + initMap() + "async defer";
+
+    $(document).ready(function(){
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      });
+  });
