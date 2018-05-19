6ccf6a38b7c97dabedf282bdc35ea9effef5de8e
Paul Baker <p.baker1@yahoo.com>
2018-05-19 10:52:49 -0500

    oops

---
+++ assets/javascript/weather.js
@@ -0,0 +1,22 @@
+var pos = {
+            lat: 35,
+            lng: 139
+          };
+
+var weather = function() {
+	var api_key = "2ff3fe9c5bf754ee9c37979fef2c77ae";
+	var cors = "https://cors-everywhere.herokuapp.com/"
+	var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&appid=" + api_key;      
+    var testURL = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=2ff3fe9c5bf754ee9c37979fef2c77ae";
+    $.ajax({
+    	url: cors + queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var weatherIcon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        console.log(weatherIcon)
+        
+    });
+};
+
+weather()
